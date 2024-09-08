2c2cfa4e826fa337a2dff99408eec585874f20c3
AndreLOBispo <abispo90@gmail.com>
2024-09-08 13:59:14 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,16 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao :"Rebeca Andrade, a joia da ginástica artística brasileira, conquistou o coração dos brasileiros com sua determinação e talento. Nascida em Guarulhos, ela superou diversas lesões para se tornar a maior medalhista olímpica do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "fadinha fada sk8"
+    },
+    {
+        titulo : "André Bispo",
+        descricao :"André Bispo, rei da governança de TI.",
+        link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ti governança rei melhor cobit itil iso"
+    }
+    
+];
+
