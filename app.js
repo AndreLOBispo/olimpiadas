65f65a0a27524fdcc8956fe5cc065691ca9ab43e
AndreLOBispo <abispo90@gmail.com>
2024-09-08 14:20:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
-function pesquisar(){
-    
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value
-    console.log(campoPesquisa)
 
-    if (!campoPesquisa){
-        section.innerHTML = "<p> Nada foi encontrado pois está vazio</p>"
-        return
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte</p>"
+        return 
     }
 
     campoPesquisa = campoPesquisa.toLowerCase()
 
-    let resultados = " ";
-    let titulo = " ";
-    let descricao = " ";
-    let tags = " "
-    
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
     for (let dado of dados) {
-            titulo = dado.titulo.toLowerCase()
-            descricao = dado.descricao.toLowerCase()
-            tags = dado.tags.toLowerCase()
-
-        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
-
-            resultados += 
-            `
-                <div class= "item-resultado">
-                    <h2>
-                        <a href="#" target="_blank">${dado.titulo}</a>
-                    </h2>
-                    <p class="descricao-meta">${dado.descricao}</p>
-                    <a href= ${dado.link} target="_blank">Mais informações sobre a Rebeca</a>
-                </div>
-            `
-           
-            }
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
         }
+    }
 
     if (!resultados) {
-        resultados = "<p> Nada foi encontrado!</p>"
-        }
+        resultados = "<p>Nada foi encontrado</p>"
+    }
 
-        section.innerHTML = resultados
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
 }
 
 
